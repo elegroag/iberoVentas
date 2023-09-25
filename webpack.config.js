3be29aa1e1ef45015310a36644051ca259c41e3c
edwin legro de comfaca <soportesistemas.comfaca@gmail.com>
2023-09-25 10:13:28 -0500

    refactor: 🔥 development @v005

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	entry: './src/main.js',
 	output: {
+		compareBeforeEmit: false,
 		path: path.resolve(__dirname, 'public/'),
 		filename: 'bundle.js',
 	},
